214c0ca9c5783c925a8fda486541004e0e8f2653
Tostka <tostka@users.noreply.github.com>
2025-01-24 09:44:37 -0600

    Update Test-Port.ps1
    # 9:11 AM 1/24/2025 added -ea 0 to the gcm's; expanded gcms; added try/catch to the Net.Sockets.TcpClient code (output friendly error fail); added CBH demo rdp users poll
            ; removed aliases 's' & 'p' (they're already usable as abbrev $server & $port, no other conflicting params )

--- Public/Test-Port.ps1
+++ Public/Test-Port.ps1
@@ -15,6 +15,8 @@ function Test-Port {
     Github      : https://github.com/tostka/verb-XXX
     Tags        : Powershell
     REVISIONS
+    # 9:11 AM 1/24/2025 added -ea 0 to the gcm's; expanded gcms; added try/catch to the Net.Sockets.TcpClient code (output friendly error fail); added CBH demo rdp users poll
+        ; removed aliases 's' & 'p' (they're already usable as abbrev $server & $port, no other conflicting params )
     # 12:28 PM 4/12/2022 prior was .net dependant, not psCore compliant: make it defer to and use the NetTCPIP:Test-NetConnection -ComputerName -Port alt, or psv6+ test-connection -targetname -tcpport, only fallback to .net when on Win and no other option avail); moved port valid to param block, rem'd out eapref; added position to params; updated CBH
     # 10:42 AM 4/15/2015 fomt cleanup, added help
     # vers: 8:42 AM 7/24/2014 added proper fail=$false
@@ -29,22 +31,39 @@ function Test-Port {
     .EXAMPLE
     PS> test-port -ComputerName hostname -Port 1234 -verbose
     Check hostname port 1234, with verbose output
+    .EXAMPLE
+    PS> 'SERVER1','SERVER2'|%{
+    PS>     $ts = $_ ;
+    PS>     write-host "`n`n==$($ts)" ;
+    PS>     if(test-port -server $ts -port 3389){
+    PS>         quser.exe /server:$ts
+    PS>     } else {
+    PS>         write-warning "$($ts):TSCPort 3389 unavailable! (check ping...)" ;
+    PS>         $ctest = $null ; 
+    PS>         TRY{$ctest = test-connection -ComputerName $ts -Count 1 -ErrorAction stop} CATCH {write-warning $Error[0].Exception.Message} 
+    PS>         if($ctest){
+    PS>             write-warning "$($ts):_Pingable_, but TSCPort 3389 unavailable!" ;
+    PS>         }else {
+    PS>             write-warning "$($ts):UNPINGABLE!" ;
+    PS>         };
+    PS>     };
+    PS> } ; 
+    Scriptblock that stacks test-port 3389 (rdp server) & test-connection against series of computers, to conditionally exec a query (run quser.exe rdp-user report)
     .LINK
     https://github.com/tostka/verb-network
     #>
+    [alias("test-portTDO")]
     PARAM(
         [parameter(Position=0,Mandatory=$true)]
-        [alias("s",'ComputerName','TargetName')]
-        [string]$Server,
+            [alias('ComputerName','TargetName')]
+            [string]$Server,
         [parameter(Position=1,Mandatory=$true)]
-        [alias("p",'TcpPort')]
-        [ValidatePattern("^(6553[0-5]|655[0-2]\d|65[0-4]\d\d|6[0-4]\d{3}|[1-5]\d{4}|[1-9]\d{0,3}|0)$")]
-        [int32]$Port
+            [alias('TcpPort')]
+            [ValidatePattern("^(6553[0-5]|655[0-2]\d|65[0-4]\d\d|6[0-4]\d{3}|[1-5]\d{4}|[1-9]\d{0,3}|0)$")]
+            [int32]$Port
     )
-    # tree down an if than and only use .net/win-specific netsockets if nothing else avail
-    if($host.version.major -ge 6 -AND (gcm test-connection)){
+    if($host.version.major -ge 6 -AND (get-command test-connection -ea STOP)){
         write-verbose "(Psv6+:using PS native:test-connection -Targetname  $($server) -tcpport $($port)...)" ; 
-        # has *ugly* output and down-state handling, so wrap & control the output
         TRY {$PortTest = test-connection -targetname $Server -tcpport $port -Count 1 -ErrorAction SilentlyContinue -ErrorVariable Err } CATCH { $PortTest = $Null } ;
         if($PortTest -ne $null ){
             write-verbose "Success" ; 
@@ -53,9 +72,8 @@ function Test-Port {
             write-verbose "Failure" ; 
             return $False;
         } ; 
-    } elseif (gcm Test-NetConnection){
+    } elseif (get-command Test-NetConnection -ea STOP){
         write-verbose "(Psv5:using NetTCPIP:Test-NetConnection -computername $($server) -port $($port)...)" ; 
-        # (test-netconnection  -computername boojum -port 3389 -verbose).PingSucceeded
         if( (Test-NetConnection -computername $Server -port $port).TcpTestSucceeded ){
             write-verbose "Success" ; 
             return $true ; 
@@ -65,8 +83,8 @@ function Test-Port {
         } ; 
     } elseif([System.Environment]::OSVersion.Platform -eq 'Win32NT'){ 
         write-verbose "(Falling back to PsWin:Net.Sockets.TcpClient)" ; 
-        $Socket = new-object Net.Sockets.TcpClient
-        $Socket.Connect($Server, $Port)
+        $Socket = new-object Net.Sockets.TcpClient ; 
+        TRY{ $Socket.Connect($Server, $Port) }CATCH{ write-warning "FAILED:($Socket).Connect(($Server), ($Port)" };
         if ($Socket.Connected){
             $Socket.Close()
             write-verbose "Success" ; 
@@ -77,8 +95,7 @@ function Test-Port {
         } # if-block end
         $Socket = $null
     } else {
-        throw "Unsupported OS/Missing depedancy module! (missing PSCore6+, NetTCPIP, .net.sockets.tcpClient)! Aborting!" ;
+        throw "Unsupported OS/Missing depedancy! (missing PSCore6+, NetTCPIP, or even .net.sockets.tcpClient)! Aborting!" ;
     } ; 
-    #} # if-E port-range valid
 } ;
 #*------^ END Function Test-Port ^------
