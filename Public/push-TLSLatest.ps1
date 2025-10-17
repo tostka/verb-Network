3e1efb41141cd0ff3f757bd204b3166cd3446c48
Tostka <tostka@users.noreply.github.com>
2025-10-17 11:52:59 -0500

    Update push-TLSLatest.ps1
    push-TLSLatest - Elevates TLS on Powershell connections to highest available local version
    * 12:46 PM 9/17/2025 spliced orig expanded CBH over, brought up to date
            * 9:05 AM 6/2/2025 expanded CBH, copied over current call from psparamt
            * 4:41 PM 5/29/2025 init (replace scriptblock in psparamt)

--- Public/push-TLSLatest.ps1
+++ Public/push-TLSLatest.ps1
@@ -1,8 +1,9 @@
-﻿
-#region PUSH_TLSLATEST ; #*------v push-TLSLatest v------
-#if(-not(gi function:push-TLSLatest -ea 0)){
-    function push-TLSLatest{
-        <#
+﻿# push-TLSLatest
+
+    #region PUSH_TLSLATEST ; #*------v push-TLSLatest v------
+    if (-not(gi function:push-TLSLatest -ea 0)) {
+        function push-TLSLatest {
+            <#
         .SYNOPSIS
         push-TLSLatest - Elevates TLS on Powershell connections to highest available local version
         .NOTES
@@ -11,21 +12,20 @@
         Website     : http://www.toddomation.com
         Twitter     : @tostka / http://twitter.com/tostka
         CreatedDate : 2025-
-        FileName    : test-ModulesAvailable.ps1
+        FileName    : push-TLSLatest.ps1
         License     : MIT License
         Copyright   : (c) 2025 Todd Kadrie
         Github      : https://github.com/tostka/verb-Network
-        Tags        : Powershell
+        Tags        : Powershell,Security,TLS,protocol,Encryption
         AddedCredit : REFERENCE
         AddedWebsite: URL
         AddedTwitter: URL
-        REVISIONS
+        REVISIONS        
+        * 12:46 PM 9/17/2025 spliced orig expanded CBH over, brought up to date
         * 9:05 AM 6/2/2025 expanded CBH, copied over current call from psparamt
         * 4:41 PM 5/29/2025 init (replace scriptblock in psparamt)
         .DESCRIPTION
-        push-TLSLatest - Elevates TLS on Powershell connections to highest available local version
-        .PARAMETER ModuleSpecifications
-        Array of semicolon-delimited module test specifications in format 'modulename;moduleurl;testcmdlet'[-ModuleSpecifications 'verb-logging;localRepo;write-log'
+        push-TLSLatest - Elevates TLS on Powershell connections to highest available local version        
         .INPUTS
         None. Does not accepted piped input.
         .OUTPUTS
@@ -34,25 +34,29 @@
         PS> push-TLSLatest ;     
         .LINK
         https://github.com/tostka/verb-Network      
-        #>
-        [CmdletBinding()]
-        PARAM() ; 
-        $CurrentVersionTlsLabel = [Net.ServicePointManager]::SecurityProtocol ; # Tls, Tls11, Tls12 ('Tls' == TLS1.0)  ;
-        write-verbose "PRE: `$CurrentVersionTlsLabel : $($CurrentVersionTlsLabel )" ;
-        # psv6+ already covers, test via the SslProtocol parameter presense
-        if ('SslProtocol' -notin (Get-Command Invoke-RestMethod).Parameters.Keys) {
-            $currentMaxTlsValue = [Math]::Max([Net.ServicePointManager]::SecurityProtocol.value__,[Net.SecurityProtocolType]::Tls.value__) ;
-            write-verbose "`$currentMaxTlsValue : $($currentMaxTlsValue )" ;
-            $newerTlsTypeEnums = [enum]::GetValues('Net.SecurityProtocolType') | Where-Object { $_ -gt $currentMaxTlsValue }
-            if($newerTlsTypeEnums){
-                write-verbose "Appending upgraded/missing TLS `$enums:`n$(($newerTlsTypeEnums -join ','|out-string).trim())" ;
-            } else {
-                write-verbose "Current TLS `$enums are up to date with max rev available on this machine" ;
-            };
-            $newerTlsTypeEnums | ForEach-Object {
-                [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor $_
+            #>
+            [CmdletBinding()]
+            PARAM() ;
+            $CurrentVersionTlsLabel = [Net.ServicePointManager]::SecurityProtocol ; # Tls, Tls11, Tls12 ('Tls' == TLS1.0)  ;
+            $smsg = "PRE: `$CurrentVersionTlsLabel : $($CurrentVersionTlsLabel )" ;
+            if($VerbosePreference -eq "Continue"){if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level VERBOSE } else{ write-verbose "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ; } ; 
+            # psv6+ already covers, test via the SslProtocol parameter presense
+            if ('SslProtocol' -notin (Get-Command Invoke-RestMethod).Parameters.Keys) {
+                $currentMaxTlsValue = [Math]::Max([Net.ServicePointManager]::SecurityProtocol.value__, [Net.SecurityProtocolType]::Tls.value__) ;
+                $smsg = "`$currentMaxTlsValue : $($currentMaxTlsValue )" ;
+                if($VerbosePreference -eq "Continue"){if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level VERBOSE } else{ write-verbose "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ; } ; 
+                $newerTlsTypeEnums = [enum]::GetValues('Net.SecurityProtocolType') | Where-Object { $_ -gt $currentMaxTlsValue }
+                if ($newerTlsTypeEnums) {
+                    $smsg = "Appending upgraded/missing TLS `$enums:`n$(($newerTlsTypeEnums -join ','|out-string).trim())" ;
+                    if($VerbosePreference -eq "Continue"){if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level VERBOSE } else{ write-verbose "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ; } ; 
+                } else {
+                    $smsg = "Current TLS `$enums are up to date with max rev available on this machine" ;
+                    if($VerbosePreference -eq "Continue"){if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level VERBOSE } else{ write-verbose "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ; } ; 
+                };
+                $newerTlsTypeEnums | ForEach-Object {
+                    [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor $_
+                } ;
             } ;
         } ;
-    } ; 
-#} ; 
-#endregion PUSH_TLSLATEST ; #*------^ END push-TLSLatest ^------
+    } ;
+    #endregion PUSH_TLSLATEST ; #*------^ END push-TLSLatest ^------
